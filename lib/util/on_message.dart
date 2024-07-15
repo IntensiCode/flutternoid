d6496825b9a36c5e995b46ee7d8a10f8a7eb483a
Daniel Lukic <daniel.lukic@gmx.net>
2024-07-15 21:52:28 +0200

    Fix a code style issue

--- lib/util/on_message.dart
+++ lib/util/on_message.dart
@@ -3,6 +3,7 @@ import '../core/messaging.dart';
 import 'auto_dispose.dart';
 
 extension AutoDisposeComponentExtensions on AutoDispose {
-  void onMessage<T extends Message>(void Function(T) callback) =>
-      autoDispose('listen-$T', messaging.listen<T>(callback));
+  void onMessage<T extends Message>(void Function(T) callback) {
+    autoDispose('listen-$T', messaging.listen<T>(callback));
+  }
 }
