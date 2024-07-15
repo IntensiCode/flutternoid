2fd0bd20cdc1db5d285b483ace109c819e64207d
Daniel Lukic <daniel.lukic@gmx.net>
2024-07-15 11:14:37 +0200

    Use single audio player for less frequent sounds

--- lib/game/soundboard_web.dart
+++ lib/game/soundboard_web.dart
@@ -9,10 +9,18 @@ import 'soundboard.dart';
 import 'storage.dart';
 
 class SoundboardImpl extends Soundboard {
+  final _preload = {
+    Sound.enemy_hit,
+    Sound.wall_hit,
+    Sound.laser_shot,
+  };
   final _sounds = <Sound, AudioPlayer>{};
   final _web_notes = <AudioPlayer>[];
   final _max_sounds = <AudioPlayer>[];
   final _last_time = <AudioPlayer, int>{};
+
+  // single player for all sounds other than _preload:
+  AudioPlayer? _shared;
 
   @override
   double? get active_music_volume => FlameAudio.bgm.audioPlayer.volume;
@@ -30,11 +38,17 @@ class SoundboardImpl extends Soundboard {
       try {
         // can't figure out why this one sound does not play in the web version ‾\_('')_/‾
         final ext = it == Sound.enemy_destroyed ? 'mp3' : 'ogg';
-        _sounds[it] = await _preload_player('${it.name}.$ext');
+        if (_preload.contains(it)) {
+          logInfo('fully preload $it');
+          _sounds[it] = await _preload_player('${it.name}.$ext');
+        } else {
+          await FlameAudio.audioCache.load('sound/${it.name}.$ext');
+        }
       } catch (e) {
         logError('failed loading $it: $e');
       }
     }
+    logInfo('fully preload notes');
     for (int i = 0; i <= 20; i++) {
       _web_notes.add(await _preload_player('note$i.ogg'));
     }
@@ -61,29 +75,42 @@ class SoundboardImpl extends Soundboard {
   @override
   Future do_play(Sound sound, double volume_factor) async {
     final play_notes = sound == Sound.wall_hit && soundboard.brick_notes;
-    final it = play_notes ? _web_notes.getOrNull(note_index ?? 0) : _sounds[sound];
-    if (it == null) {
+    var it = play_notes ? _web_notes.getOrNull(note_index ?? 0) : _sounds[sound];
+    if (it == null && _preload.contains(sound)) {
       logError('null sound: $sound');
       preload();
       return;
     }
 
-    final last_played_at = _last_time[it] ?? 0;
-    final now = DateTime.timestamp().millisecondsSinceEpoch;
-    if (now < last_played_at + 100) return;
-    _last_time[it] = now;
+    if (it == null) {
+      final ext = sound == Sound.enemy_destroyed ? 'mp3' : 'ogg';
+      if (_shared == null) {
+        _shared = await FlameAudio.play('sound/${sound.name}.$ext', volume: super.sound);
+        await _shared?.setReleaseMode(ReleaseMode.stop);
+        await _shared?.setPlayerMode(PlayerMode.lowLatency);
+      } else {
+        await _shared?.stop();
+        await _shared?.setSourceAsset('sound/${sound.name}.$ext');
+        await _shared?.resume();
+      }
+    } else {
+      final last_played_at = _last_time[it] ?? 0;
+      final now = DateTime.timestamp().millisecondsSinceEpoch;
+      if (now < last_played_at + 100) return;
+      _last_time[it] = now;
 
-    _max_sounds.removeWhere((it) => it.state != PlayerState.playing);
-    if (_max_sounds.length > 10) {
-      if (dev) logWarn('sound killed - overload');
-      final fifo = _max_sounds.removeAt(0);
-      await fifo.stop();
+      _max_sounds.removeWhere((it) => it.state != PlayerState.playing);
+      if (_max_sounds.length > 10) {
+        if (dev) logWarn('sound killed - overload');
+        final fifo = _max_sounds.removeAt(0);
+        await fifo.stop();
+      }
+
+      if (it.state != PlayerState.stopped) await it.stop();
+      await it.setVolume(volume_factor * super.sound);
+      await it.resume();
+      note_index = 0;
     }
-
-    if (it.state != PlayerState.stopped) await it.stop();
-    await it.setVolume(volume_factor * super.sound);
-    await it.resume();
-    note_index = 0;
   }
 
   @override
