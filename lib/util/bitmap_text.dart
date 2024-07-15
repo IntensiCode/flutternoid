b65f060d1c0cea6d7f041d750e82f89b7e65bb40
Daniel Lukic <daniel.lukic@gmx.net>
2024-07-15 21:17:06 +0200

    Revert "Use snapshots for BitmapButton and BitmapText"
    This reverts commit 3e1a6ed05708e538f4c169d49a9c1d6d7cfb271a.

--- lib/util/bitmap_text.dart
+++ lib/util/bitmap_text.dart
@@ -1,10 +1,7 @@
-import 'dart:math';
 import 'dart:ui';
 
-import 'package:dart_minilog/dart_minilog.dart';
 import 'package:flame/components.dart';
 
-import '../core/common.dart';
 import 'bitmap_font.dart';
 import 'fonts.dart';
 
@@ -12,7 +9,6 @@ class BitmapText extends PositionComponent with HasPaint, HasVisibility {
   final String text;
   final BitmapFont font;
   final double fontScale;
-  Image? _snapshot;
 
   BitmapText({
     required this.text,
@@ -23,7 +19,6 @@ class BitmapText extends PositionComponent with HasPaint, HasVisibility {
     Anchor anchor = Anchor.topLeft,
   })  : font = font ?? mini_font,
         fontScale = scale {
-    paint = pixelPaint();
     if (tint != null) this.tint(tint);
     this.position.setFrom(position);
     this.font.scale = fontScale;
@@ -33,52 +28,18 @@ class BitmapText extends PositionComponent with HasPaint, HasVisibility {
     final y = anchor.y * h;
     this.position.x -= x;
     this.position.y -= y;
-    size.setValues(max(0, w), max(0, h));
-  }
-
-  @override
-  void onRemove() {
-    super.onRemove();
-    _snapshot?.dispose();
+    size.setValues(w, h);
   }
 
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-    if (size.x == 0 || size.y == 0) {
-      logError('invalid size for $this, size $size, text $text');
-      removeFromParent();
-      return;
-    }
-    try {
-      _snapshot ??= _render_snapshot();
-      canvas.scale(fontScale);
-      canvas.drawImage(_snapshot!, Offset.zero, paint);
-    } catch (it, trace) {
-      logError(size);
-      logError('snapshot failed for $this, size $size, text $text: $it', trace);
-      removeFromParent();
-    }
-  }
-
-  Image _render_snapshot() {
-    final recorder = PictureRecorder();
-    final canvas = Canvas(recorder);
-    final paint = pixelPaint();
-
     font.paint.color = paint.color;
     font.paint.colorFilter = paint.colorFilter;
     font.paint.filterQuality = FilterQuality.none;
     font.paint.isAntiAlias = false;
     font.paint.blendMode = paint.blendMode;
-    font.scale = 1; // fontScale;
+    font.scale = fontScale;
     font.drawString(canvas, 0, 0, text);
-
-    final width = size.x / fontScale;
-    final height = size.y / fontScale;
-    final picture = recorder.endRecording();
-    final result = picture.toImageSync(width.toInt(), height.toInt());
-    picture.dispose();
-    return result;
   }
 }
