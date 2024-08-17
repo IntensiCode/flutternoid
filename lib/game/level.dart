c46140a46afbe89dfb65c26b80f84ecba87b335e
Daniel Lukic <daniel.lukic@gmx.net>
2024-08-17 13:53:05 +0200

    Revert "Use SpriteBatch for bricks"
    This reverts commit 714f1ce643f6cea849faa0528c21ed12629de9e1.

--- lib/game/level.dart
+++ lib/game/level.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-import 'dart:ui';
 
 import 'package:collection/collection.dart' hide IterableExtension;
 import 'package:dart_minilog/dart_minilog.dart';
@@ -7,7 +6,6 @@ import 'package:flame/components.dart';
 import 'package:flame/extensions.dart';
 import 'package:flame/sprite.dart';
 import 'package:flame_tiled/flame_tiled.dart';
-import 'package:flutter/foundation.dart';
 import 'package:kart/kart.dart';
 import 'package:supercharged/supercharged.dart';
 
@@ -126,8 +124,6 @@ class Level extends PositionComponent with AutoDispose, GameContext, HasPaint {
     paint = pixelPaint();
 
     sprites = atlas.sheetIWH('game_blocks.png', visual.brick_width, visual.brick_height, spacing: 1);
-
-    _batch = SpriteBatch(sprites.image, useAtlas: !kIsWeb);
 
     onMessage<EnterRound>((_) => _reset());
     onMessage<LevelComplete>((_) => _reset(full_clear: false));
@@ -382,8 +378,8 @@ class Level extends PositionComponent with AutoDispose, GameContext, HasPaint {
   void _render_active(Canvas canvas) {
     paint.opacity = 1;
 
-    for (var (y, row) in brick_rows.indexed) {
-      for (var (x, brick) in row.indexed) {
+    for (var row in brick_rows) {
+      for (var brick in row) {
         if (brick == null) continue;
 
         Sprite sprite;
@@ -396,28 +392,17 @@ class Level extends PositionComponent with AutoDispose, GameContext, HasPaint {
         } else {
           sprite = sprites.getSpriteById(brick.id.index);
         }
-
-        final trans = _cached_transforms[x + y * 15] ??= brick.topLeft.transform;
-        _batch.addTransform(source: sprite.src, transform: trans);
+        sprite.render(canvas, position: brick.topLeft, overridePaint: paint);
 
         if (brick.sweep_progress > 0) {
           final progress = 1 - brick.sweep_progress.clamp(0.0, 1.0);
           final frame = (sprites.columns - 1) * progress;
           sprite = sprites.getSpriteById(25 + frame.round().clamp(0, sprites.columns));
-          _batch.addTransform(source: sprite.src, transform: trans);
+          sprite.render(canvas, position: brick.topLeft, overridePaint: paint);
         }
       }
     }
-
-    _batch.render(canvas, paint: _batch_paint);
-    _batch.clear();
   }
-
-  final _batch_paint = pixelPaint();
-
-  final _cached_transforms = <int, RSTransform>{};
-
-  late final SpriteBatch _batch;
 }
 
 extension on TiledComponent {
@@ -433,15 +418,4 @@ extension on TiledComponent {
     }
     return SpawnMode.values.firstWhere((it) => it.name == spawn_mode);
   }
-}
-
-extension Vector2Extensions on Vector2 {
-  RSTransform get transform => RSTransform.fromComponents(
-        rotation: 0,
-        scale: 1.0,
-        anchorX: 0,
-        anchorY: 0,
-        translateX: x,
-        translateY: y,
-      );
 }
